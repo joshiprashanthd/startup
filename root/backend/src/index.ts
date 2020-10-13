9947397dd6b9da3b1e535f8ca61197f14e566147
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-13 19:49:10 +0530

    FEAT: Add ApolloServer implementation

--- root/backend/src/index.ts
+++ root/backend/src/index.ts
@@ -1,5 +1,9 @@
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import mongoose from "mongoose";
+
+//local
+import { TypeDefs, Resolvers } from "./Types";
 
 mongoose
 	.connect(
@@ -13,3 +17,14 @@ mongoose
 const app = express();
 
 app.disable("x-powered-by");
+
+const server = new ApolloServer({
+	typeDefs: TypeDefs,
+	resolvers: Resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+	console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+});
