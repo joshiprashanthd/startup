4958f6988655069ad6a3ef744bc80cb8eb7c623e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-13 19:37:42 +0530

    FIX: Omit the undefine type definitions

--- root/backend/src/Types/User/TypeDef/index.ts
+++ root/backend/src/Types/User/TypeDef/index.ts
@@ -5,4 +5,4 @@ import MutationDef from "./mutation";
 import FragmentDef from "./fragment";
 import TypeDef from "./type";
 
-export default [InputDef, SubscriptionDef, QueryDef, MutationDef, FragmentDef, TypeDef];
+export default [InputDef, QueryDef, MutationDef, TypeDef];
