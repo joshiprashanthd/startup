c71d15e11814c374731e1325c2e7263a406b73c0
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-13 20:35:51 +0530

    FEAT: Add id field to type User

--- root/backend/src/Types/User/TypeDef/type.ts
+++ root/backend/src/Types/User/TypeDef/type.ts
@@ -2,11 +2,10 @@ import { gql } from "apollo-server-express";
 import { DocumentNode } from "graphql";
 
 export default gql`
-
-  type User {
-    email: String!
-    password: String
-    handler: String!
-  }
-
+	type User {
+		id: ID!
+		email: String!
+		password: String
+		handler: String!
+	}
 ` as DocumentNode;
