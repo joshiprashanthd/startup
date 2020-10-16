cb6710e4ac3a896c7e6ec2b5b60222901c079bfb
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-16 13:47:06 +0530

    FIX: Change type to string to avoid error
    If process.env.SESSION_LIFETIME is null, then default value was integer
    type of 1800000 which causes error when used in parseInt
    function.Therefore it changed to string type

--- root/backend/src/utils/env/session.config.ts
+++ root/backend/src/utils/env/session.config.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export default {
 	sessionName: process.env.SESSION_NAME ?? "",
-	sessionLifetime: process.env.SESSION_LIFETIME ?? 1800000,
+	sessionLifetime: process.env.SESSION_LIFETIME ?? "1800000",
 	sessionSecret: process.env.SESSION_SECRET ?? "secret"
 };
