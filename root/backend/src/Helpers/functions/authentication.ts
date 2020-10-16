80d8bdcbf291d6ccb745e0523eb1c145bbea0175
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-16 19:28:17 +0530

    FEAT: Add authentication helper functions
    ensureSignedIn: To check whether user already signed in or not
    
    ensureSignedOut: Check user is signed out or not
    
    attemptSignIn: Requires email and password and perform all checks before
    sign in
    
    attemptsSignOut: Destroys the current running session.

---
+++ root/backend/src/Helpers/functions/authentication.ts
@@ -0,0 +1,42 @@
+import { AuthenticationError } from "apollo-server-express";
+
+//local
+import User from "../../Types/User/Model/model";
+import { SessionConfig } from "../../utils/config";
+
+export const ensureSignedIn = req => {
+	if (!req.session.userId)
+		throw new AuthenticationError("You must be signed in.");
+};
+
+export const ensureSignedOut = req => {
+	if (req.session.userId)
+		throw new AuthenticationError("You must be signed out.");
+};
+
+export const attemptSignIn = async (
+	email: string,
+	password: string,
+	req: any
+) => {
+	const user = await User.findOne({ email });
+
+	if (!user) throw new AuthenticationError("Cannot find email");
+
+	if (!(await user.comparePassword(password)))
+		throw new AuthenticationError("Incorrect password.");
+
+	req.session.userId = user.id;
+
+	return user;
+};
+
+export const attemptSignOut = (req: any, res: any) => {
+	return new Promise((resolve, reject) => {
+		req.session.destroy(err => {
+			if (err) reject(err);
+			res.clearCookie(SessionConfig.sessionName);
+			resolve(true);
+		});
+	});
+};
