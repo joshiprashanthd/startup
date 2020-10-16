f09032d10fd640d0fa7a753bc12a411a64731cf9
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-16 13:53:15 +0530

    FEAT: add new mutation signIn and signOut

--- root/backend/src/Types/User/TypeDef/mutation.ts
+++ root/backend/src/Types/User/TypeDef/mutation.ts
@@ -4,5 +4,7 @@ import { DocumentNode } from "graphql";
 export default gql`
 	extend type Mutation {
 		createUser(input: StrictUserInput!): User!
+		signIn(email: String!, password: String!): User!
+		signOut: Boolean!
 	}
 ` as DocumentNode;
