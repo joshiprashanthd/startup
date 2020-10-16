68c50157d9d6cc3ce7c32958685155b36f0571db
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-16 13:57:23 +0530

    FEAT: Add resolvers for signIn and signOut mutation
    signIn resolver will throw authentication error if userId is stored on
    the current session cookie. And signOut will throw error when userId is
    not available from session.cookie.

--- root/backend/src/Types/User/Resolvers/mutation.ts
+++ root/backend/src/Types/User/Resolvers/mutation.ts
@@ -1,5 +1,9 @@
+import { AuthenticationError } from "apollo-server-express";
+
+// local
 import User from "../Model/model";
 import { IStrictUserInput } from "../TypeDef";
+import SessionConfig from "../../../utils/env/session.config";
 
 export default {
 	Mutation: {
@@ -9,10 +13,45 @@ export default {
 			context: any,
 			info: any
 		) => {
-			console.log(args.input);
-			const user = await User.create<IStrictUserInput>(args.input);
-			console.log(user);
+			return await User.create<IStrictUserInput>(args.input);
+		},
+
+		signIn: async (
+			parent: any,
+			{ email, password }: { email: string; password: string },
+			{ req },
+			info: any
+		) => {
+			if (req.session.userId) {
+				throw new AuthenticationError("You must be signed out.");
+			}
+
+			const user = await User.findOne({ email: email });
+
+			if (!user) {
+				throw new AuthenticationError("Cannot find email");
+			}
+
+			if (!(await user.comparePassword(password))) {
+				throw new AuthenticationError("Incorrect password.");
+			}
+
+			req.session.userId = user.id;
 			return user;
+		},
+
+		signOut: async (parent: any, args: any, { req, res }, info: any) => {
+			if (!req.session.userId) {
+				throw new AuthenticationError("You must be signed in!");
+			}
+
+			return new Promise((resolve, reject) => {
+				req.session.destroy(err => {
+					if (err) reject(err);
+					res.clearCookie(SessionConfig.sessionName);
+					resolve(true);
+				});
+			});
 		}
 	}
 };
