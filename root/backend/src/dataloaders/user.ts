d5131a3680564495b6419fcf303e0c26771213f1
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-17 17:09:37 +0530

    FIX: Use mongoose.model function to access User model
    Directly accessing User model from ./user/model directoryis compiling
    the same User Schema twice which leads to error.
    
    Using mongoose.model function helps to access the previously compiled
    model

--- root/backend/src/dataloaders/user.ts
+++ root/backend/src/dataloaders/user.ts
@@ -1,10 +1,12 @@
 import Dataloader from "dataloader";
+import mongoose from "mongoose";
 import { groupBy, map } from "ramda";
 
-//
-import { IUserDocument, User } from "../entities/user/model";
+//local
+import { IUserDocument } from "../entities/user/model";
 
 async function batchUsers(ids: string[]): Promise<IUserDocument[]> {
+	const User = mongoose.model("User");
 	const users = await User.find({ _id: { $in: ids } });
 	const groupedUsers = groupBy(user => user.id, users);
 	return map(id => groupedUsers[id], ids);
