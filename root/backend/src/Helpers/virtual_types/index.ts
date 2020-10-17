da8aaf88a309939cf12bb5ccd9bebd16731fe835
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-17 17:18:37 +0530

    CHORE: Deleted virtual_types

--- root/backend/src/Helpers/virtual_types/index.ts
+++
@@ -1,8 +0,0 @@
-interface VirtualType {
-  type: any
-  required: boolean
-}
-
-export const StringRequired: VirtualType = { type: String, required: true };
-export const BooleanRequired: VirtualType = { type: Boolean, required: true };
-export const NumberRequired: VirtualType = { type: Number, required: true };
