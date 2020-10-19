073e8907fa90ca1490f281c48f30e2bc4747895c
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-19 22:24:53 +0530

    CHORE: Add custom IMapUserType and use dataloader in mapper
    Created IMapUserType interface to match return type of mapper function
    mapUser, in which the returned object has async function interests which
    returns IFieldType objects on demand.
    
    It's a good practise to assign a async function which to property which
    returns list of objects. In this way, we are not loading the whole
    object, instead we are provinding a function, on called, returns the
    desired output.

--- root/backend/src/entities/User/mapper.ts
+++ root/backend/src/entities/User/mapper.ts
@@ -1,7 +1,18 @@
+import { IContext } from "../../types";
+import { mapFields } from "../field/mapper";
+import { IFieldDocument } from "../field/model";
+import { IFieldType } from "../field/typedef";
 import { IUserDocument } from "./Model";
 import { IUserType } from "./TypeDef";
 
-export const mapUser = (user: IUserDocument): IUserType => ({
+interface IMapUserType extends Omit<IUserType, "interests"> {
+	interests: () => Promise<(IFieldType | Error)[]>;
+}
+
+export const mapUser = (
+	user: IUserDocument,
+	context: IContext
+): IMapUserType => ({
 	id: user.id,
 	name: user.name,
 	email: user.email,
@@ -11,8 +22,16 @@ export const mapUser = (user: IUserDocument): IUserType => ({
 	birthDate: user.birthDate,
 	isOnline: user.isOnline,
 	lastActive: user.lastActive,
-	verifiedAccount: user.verifiedAccount
+	verifiedAccount: user.verifiedAccount,
+	interests: async () =>
+		mapFields(
+			(await context.dataloaders.fieldLoader.loadMany(
+				user.interests
+			)) as IFieldDocument[]
+		)
 });
 
-export const mapUsers = (users: IUserDocument[]): IUserType[] =>
-	users.map(user => mapUser(user));
+export const mapUsers = (
+	users: IUserDocument[],
+	context: IContext
+): IMapUserType[] => users.map(user => mapUser(user, context));
