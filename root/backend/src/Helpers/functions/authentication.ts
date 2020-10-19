45c10c9a685228b065ff11a009b9d33ee6ef227b
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-19 22:20:38 +0530

    REFACTOR: Use context instead of req in authentication functions
    Using context would be good way to access req object since context is
    typed with interface IContext.

--- root/backend/src/Helpers/functions/authentication.ts
+++ root/backend/src/Helpers/functions/authentication.ts
@@ -2,22 +2,23 @@ import { ApolloError, AuthenticationError } from "apollo-server-express";
 
 //local
 import { User } from "../../entities/user/model";
+import { IContext } from "../../types";
 import { SessionConfig } from "../../utils/config";
 
-export const ensureSignedIn = req => {
-	if (!req.session.userId)
+export const ensureSignedIn = (context: IContext) => {
+	if (!context.req.session.userId)
 		throw new AuthenticationError("You must be signed in.");
 };
 
-export const ensureSignedOut = req => {
-	if (req.session.userId)
+export const ensureSignedOut = (context: IContext) => {
+	if (context.req.session.userId)
 		throw new AuthenticationError("You must be signed out.");
 };
 
 export const attemptSignIn = async (
 	email: string,
 	password: string,
-	req: any
+	context: IContext
 ) => {
 	const user = await User.findOne({ email });
 
@@ -39,23 +40,23 @@ export const attemptSignIn = async (
 		}
 	);
 
-	req.session.userId = user.id;
+	context.req.session.userId = user.id;
 
 	return result;
 };
 
-export const attemptSignOut = (req: any, res: any) => {
+export const attemptSignOut = (context: IContext) => {
 	return new Promise((resolve, reject) => {
 		User.updateOne(
-			{ _id: req.session.userId },
+			{ _id: context.req.session.userId },
 			{ isOnline: false },
 			(err, res) => {
 				if (err) reject(err);
 			}
 		);
-		req.session.destroy(err => {
+		context.req.session.destroy(err => {
 			if (err) reject(err);
-			res.clearCookie(SessionConfig.sessionName);
+			context.res.clearCookie(SessionConfig.sessionName);
 			resolve(true);
 		});
 	});
