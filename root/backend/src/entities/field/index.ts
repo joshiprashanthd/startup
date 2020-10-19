baf19fe1fa8864b09535fc88c2e4b1f534c0effc
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-19 16:13:51 +0530

    CHORE: Export Field types, typedef and resolvers

---
+++ root/backend/src/entities/field/index.ts
@@ -0,0 +1,2 @@
+export { default as FieldResolvers } from "./resolvers";
+export { default as FieldTypeDef } from "./typedef";
---
+++ root/backend/src/entities/field/typedef/index.ts
@@ -0,0 +1,12 @@
+import InputDef from "./input";
+import TypeDef from "./type";
+import MutationDef from "./mutation";
+
+export {
+	IFieldType,
+	IFieldIdInput,
+	IStrictFieldInput,
+	ILooseFieldInput
+} from "./types";
+
+export default [InputDef, TypeDef, MutationDef];
