4c73d45478a04e9e28d191b5045269859cf2a7f8
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-20 20:28:17 +0530

    FEAT: add @private directive
    @private directive is used on those fields which should return null when
    non-owner try to access it.
    
    In this way, we can ensure that private information does no accessed
    maliciously.

--- root/backend/src/entities/customDirectives/typedef/index.ts
+++ root/backend/src/entities/customDirectives/typedef/index.ts
@@ -3,4 +3,5 @@ import { gql } from "apollo-server-express";
 export default gql`
 	directive @guest on FIELD_DEFINITION
 	directive @auth on FIELD_DEFINITION
+	directive @private on FIELD_DEFINITION
 `;
