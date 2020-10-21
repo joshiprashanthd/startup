d848003a3966b18bfeeface54f1a624dfe6ee485
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-21 19:47:18 +0530

    FEAT: Setup Gmail emailer
    This Gmail emailer is used to send verification email. Credentials are
    provided in the .env file.
    
    sendVerificationEmail is now a sync function, which calls
    emailer.sendMail without await keyword. This idone because, sending
    email takes too much time which results in poorer sign up interaction on
    the frontend side.
    
    sendVerificationEmail now returns token only.
    
    A new template html is added to verification email which contains a
    confirm button.
    
    EDIT: accessing token without square brackets

--- root/backend/src/Helpers/functions/sendVerificationEmail.ts
+++ root/backend/src/Helpers/functions/sendVerificationEmail.ts
@@ -5,27 +5,78 @@ import { IUserDocument } from "../../entities/user/model";
 import { TokenConfig } from "../../config";
 import emailer from "../emailer";
 
-export const sendVerificationEmail = async (user: IUserDocument) => {
+export const sendVerificationEmail = (user: IUserDocument) => {
 	const token = jwt.sign(
 		{ userId: user.id, email: user.email },
 		TokenConfig.tokenSecret,
 		{ expiresIn: parseInt(TokenConfig.tokenExpiry) }
 	);
 
-	const transporter = await emailer();
-	const mailInfo = await transporter.sendMail({
+	emailer.sendMail({
 		from: '"Prashant Joshi (CEO)" <no-reply@collabs.com>',
 		to: `${user.email}`,
 		subject: "Please verify your email address",
 		html: `
-    <h1>Thank you for registering</h1>
-    <p>Verify your email by clicking on this link</p>
-    <br>
-    <a href="http:localhost:4000/auth/verify-email/${user.id}-${token}">
-      Verify you email
-    </a>
+		<html>
+		<head>
+			<style>
+				* {
+					font-family: "Proxima Nova";
+				}
+				
+				div.main {
+					width: fit-content;
+					margin: 2rem auto;
+				}
+					
+				p {
+					font-size: 1rem;
+				}
+				
+				a {
+					margin: 1rem auto;
+					display: inline-block;
+					padding: 1rem;
+					text-decoration: none;
+					border-radius: 4pt;
+					background-color: blue;
+					font-weight: bold;
+					color: white;
+				}
+				
+				a:hover {
+					background-color: darkblue;
+					color: white;
+				}
+
+				p.footer {
+					font-size: .8rem;
+					color: grey;
+				}
+			</style>
+		</head>
+		<body>
+			<div class="main">
+			<h1>
+				Thank you for registering for Collabs.io
+			</h1>
+			<hr>
+			<p>
+				Hey,<br>
+				I am the CEO of <b>Collabs.io</b>, and you have a great journey ahead with us.<br>
+				You can confirm your email by cliking the button below.
+			</p>
+			<a href="http:localhost:4000/auth/verify-email/${user.id}-${token}">
+				Confirm your email
+			</a>
+			<p class="footer">
+				Collabs.io
+			</p>
+		</div>
+		</body>
+	</html>	
     `
 	});
 
-	return [token, mailInfo];
+	return token;
 };
