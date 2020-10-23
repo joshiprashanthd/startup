ff1a320e62d2a30610d399971e94acf17f1cbb7e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-23 18:54:56 +0530

    FIX: Return first element of groupedUsers[id]
    groupedUsers stores list of objects satisfy the given predicate. Since user id is unique, there must be only one element in matched ids. And also return entity must be list of objects, and not list of list of objects. Therefore returning first element is the right way to do this.

--- root/backend/src/dataloaders/user.ts
+++ root/backend/src/dataloaders/user.ts
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 import { groupBy, map } from "ramda";
 
 //local
-import { IUserDocument } from "../entities/user/model";
+import { IUserDocument, IUserModel } from "../entities/user/model";
 
 async function batchUsers(ids: string[]): Promise<IUserDocument[]> {
-	const User = mongoose.model("User");
+	const User = mongoose.model<IUserDocument, IUserModel>("User");
 	const users = await User.find({ _id: { $in: ids } });
 	const groupedUsers = groupBy(user => user.id, users);
-	return map(id => groupedUsers[id], ids);
+	return map(id => groupedUsers[id][0], ids);
 }
 
 export default () => new Dataloader<string, IUserDocument>(batchUsers);
