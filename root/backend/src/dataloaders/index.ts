3c0e73c57e8cb3802ce352162a7f1f5466c17f57
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-27 11:06:22 +0530

    CHORE: Export MessageLoader

--- root/backend/src/dataloaders/index.ts
+++ root/backend/src/dataloaders/index.ts
@@ -1,12 +1,15 @@
 import UserLoader from "./user";
 import SkillLoader from "./skill";
+import MessageLoader from "./message";
 
 export interface IDataloaders {
 	userLoader: ReturnType<typeof UserLoader>;
 	skillLoader: ReturnType<typeof SkillLoader>;
+	messageLoader: ReturnType<typeof MessageLoader>;
 }
 
 export default {
 	userLoader: UserLoader(),
-	skillLoader: SkillLoader()
+	skillLoader: SkillLoader(),
+	messageLoader: MessageLoader()
 } as IDataloaders;
