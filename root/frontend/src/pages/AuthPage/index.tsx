7899440a849fcb75954c506d3dfda08f2424c541
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-10-31 12:32:30 +0530

    CHORE: Use Text.Heading instead Heading component

--- root/frontend/src/pages/AuthPage/index.tsx
+++ root/frontend/src/pages/AuthPage/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { SignUpCard } from "../../components/SignUpCard";
-import { Heading, HeadingSize } from "../../mini-components/Heading";
+import { Text } from "../../mini-components/Text";
+import { HeadingSize } from "../../mini-components/Text/Heading";
 import { StyledContent, StyledPage } from "./styles";
 
 export default function AuthPage() {
   return (
     <StyledPage>
       <StyledContent>
-        <Heading fontSize={HeadingSize.NORMAL}>
+        <Text.Heading fontSize={HeadingSize.NORMAL}>
           collabs lets you build projects and collaborate with others as a team
-        </Heading>
+        </Text.Heading>
       </StyledContent>
       <StyledContent>
         <SignUpCard></SignUpCard>
