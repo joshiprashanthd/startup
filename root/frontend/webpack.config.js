101fa17ad214df2ea41d982a748208ec8e05be5b
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-04 12:32:00 +0530

    CHORE: Update HtmlWebpackPlugin path

--- root/frontend/webpack.config.js
+++ root/frontend/webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src/public", "index.html"),
+      template: path.join(__dirname, "public", "index.html"),
       inject: "body",
     }),
   ],
