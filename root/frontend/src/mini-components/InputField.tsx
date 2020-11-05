74355f63060077a6429c1670a728927878d37ac4
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-05 23:23:53 +0530

    CHORE: Create InputField mini component
    InputField takes label and inputChange handler to render the clean input field with input label.
    
    It can also show error message. showError and errorMessage are used in case some validation is required on the input field.

---
+++ root/frontend/src/mini-components/InputField.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface IProps {
+  label?: string;
+  secondaryLabel?: string;
+  inputType?: string;
+  showError?: boolean;
+  errorMessage?: string;
+  onInputChange?: (value: string) => void;
+}
+
+export const InputField: React.FC<IProps> = function ({
+  label,
+  secondaryLabel,
+  inputType = "text",
+  showError = false,
+  errorMessage,
+  onInputChange,
+}) {
+  return (
+    <div className="my-4">
+      <Label secondary={secondaryLabel}>{label}</Label>
+      <Input type={inputType} onChange={onInputChange} />
+      {showError && <Error>{errorMessage}</Error>}
+    </div>
+  );
+};
+
+const Label = function (props: any) {
+  return (
+    <p className="mb-2 text-sm font-semibold">
+      {props.children}{" "}
+      <span className="text-xs text-gray-500">{props.secondary}</span>
+    </p>
+  );
+};
+
+const Input = function (props: any) {
+  const onChangeHandler = (event: any) => {
+    props.onChange(event.currentTarget.value);
+  };
+
+  return (
+    <input
+      className="w-full p-2 duration-200 border border-gray-400 rounded outline-none focus:border-transparent active:border-none focus:shadow-outline "
+      spellCheck="false"
+      type={props.type}
+      onChange={onChangeHandler}
+    />
+  );
+};
+
+const Error = function (props: any) {
+  return <p className="text-sm text-red-600">{props.children}</p>;
+};
