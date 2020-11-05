bfe0cae7f6f9ec24eda8134f8d4e1d28663a3938
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-05 11:19:42 +0530

    CHORE: Minor reformating.

--- root/frontend/src/index.tsx
+++ root/frontend/src/index.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./styles/styles.css";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql ",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
 
