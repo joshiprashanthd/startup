8c1b2abd9cf949bdc256d85e4e0f4cb6768f5d8b
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-05 20:25:56 +0530

    CHORE: Setup react router on App component
    Right now, only two routes are added /auth and /home.
    
    AuthContext is passed down from App.

--- root/frontend/src/App.tsx
+++ root/frontend/src/App.tsx
@@ -1,6 +1,31 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+//local
+import AuthContext from "./contexts/AuthContext";
 import { AuthPage } from "./pages/AuthPage";
+import { HomePage } from "./pages/HomePage";
 
 export default function App() {
-  return <AuthPage />;
+  return (
+    <AuthContext.Provider
+      value={{ user: null, signIn: null, signOut: null, signUp: null }}
+    >
+      <Router>
+        <Switch>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
+  );
 }
