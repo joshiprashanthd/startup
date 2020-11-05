1f04f727bd211ab535493b8911b3436a2b2281d9
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-06 00:03:16 +0530

    CHORE: Add input validation to sign up card

--- root/frontend/src/components/SignUpCard.tsx
+++ root/frontend/src/components/SignUpCard.tsx
@@ -1,5 +1,11 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
+import {
+  validateEmail,
+  validateHandler,
+  validateName,
+  validatePassword,
+} from "../helpers/validators";
 import { InputField } from "../mini-components/InputField";
 import { Loader } from "./Loader";
 
@@ -19,39 +25,80 @@ const CREATE_USER_MUTATION = gql`
 export const SignUpCard = function (props: any) {
   const [createUser, { loading }] = useMutation(CREATE_USER_MUTATION);
 
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [handler, setHandler] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [handler, setHandler] = useState("");
+
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
 
   const onSignUp = () => {
-    createUser({
-      variables: {
-        input: {
-          accountInfo: {
-            email,
-            password,
-            handler,
-            name,
+    let errorOccurred = false;
+
+    setValidationErrors([]);
+    if (!validateEmail(email)) {
+      errorOccurred = true;
+      setValidationErrors((prev) => [...prev, "email"]);
+    }
+    if (!validatePassword(password)) {
+      errorOccurred = true;
+      setValidationErrors((prev) => [...prev, "password"]);
+    }
+    if (!validateHandler(handler)) {
+      errorOccurred = true;
+      setValidationErrors((prev) => [...prev, "handler"]);
+    }
+    if (!validateName(name)) {
+      errorOccurred = true;
+      setValidationErrors((prev) => [...prev, "name"]);
+    }
+
+    if (!errorOccurred) {
+      createUser({
+        variables: {
+          input: {
+            accountInfo: {
+              email,
+              password,
+              handler,
+              name,
+            },
           },
         },
-      },
-    })
-      .then((resData) => {
-        console.log(resData);
       })
-      .catch((err) => console.log(err));
+        .then((resData) => {
+          console.log(resData);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
     <div className="w-7/12 p-4 rounded-lg shadow-md">
-      <InputField label="Name" onInputChange={setName} />
-      <InputField label="Handler" onInputChange={setHandler} />
-      <InputField label="Email" onInputChange={setEmail} />
+      <InputField
+        label="Name"
+        onInputChange={setName}
+        showError={validationErrors.includes("name")}
+        errorMessage="Name must contain only alphabets"
+      />
+      <InputField
+        label="Handler"
+        onInputChange={setHandler}
+        showError={validationErrors.includes("handler")}
+        errorMessage="Handler must be alphanumeric"
+      />
+      <InputField
+        label="Email"
+        onInputChange={setEmail}
+        showError={validationErrors.includes("email")}
+        errorMessage="Email is not valid"
+      />
       <InputField
         label="Password"
         inputType="password"
         onInputChange={setPassword}
+        showError={validationErrors.includes("password")}
+        errorMessage="Password must contain at least 8 characters and at least one digit"
       />
 
       <Button
