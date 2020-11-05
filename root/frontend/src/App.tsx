57da3dc9ea1865d9db761f8de4e0851a9e739472
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-05 20:30:17 +0530

    CHORE: Redirect to auth

--- root/frontend/src/App.tsx
+++ root/frontend/src/App.tsx
@@ -18,6 +18,7 @@ export default function App() {
     >
       <Router>
         <Switch>
+          <Redirect exact from="/" to="/auth" />
           <Route path="/auth">
             <AuthPage />
           </Route>
