77a891829c28500e79e8275fa3aac350e3898c27
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-05 20:08:47 +0530

    CHORE: Create dummy HomePage component

---
+++ root/frontend/src/pages/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomePage = function (props: any) {
+  return (
+    <div className="w-full min-h-screen">
+      <h1 className="text-4xl font-display"> The home page</h1>
+    </div>
+  );
+};
