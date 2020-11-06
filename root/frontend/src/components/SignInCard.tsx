2701810f0c16b1c4ee12fe391745bf0a212fdf36
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-06 16:12:05 +0530

    CHORE: Add validation and sign in request to SignInCard
    Now button is disabled if email field or password field is empty
    
    Add a new Alert component which is used when email or password is incorrect.

--- root/frontend/src/components/SignInCard.tsx
+++ root/frontend/src/components/SignInCard.tsx
@@ -1,14 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+//local
 import { InputField } from "../mini-components/InputField";
+import { Loader } from "./Loader";
+
+const SIGN_IN_MUTATION = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      id
+      accountInfo {
+        name
+        handler
+        email
+      }
+    }
+  }
+`;
 
 export const SignInCard = function (props: any) {
+  const [signIn, { loading }] = useMutation(SIGN_IN_MUTATION);
+
+  const [showError, setShowError] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSignIn = () => {
+    setShowError(false);
+    signIn({
+      variables: {
+        email,
+        password,
+      },
+    })
+      .then((resData) => {
+        console.log(resData);
+      })
+      .catch((err) => {
+        setShowError(true);
+      });
+  };
+
   return (
     <div className="w-7/12 p-4 rounded-lg shadow-md">
-      <InputField label="Email" />
-      <InputField label="Password" inputType="password" />
-      <Button>Sign In</Button>
+      {showError && <Alert>Incorrect email or password</Alert>}
+      <InputField label="Email" onInputChange={setEmail} />
+      <InputField
+        label="Password"
+        inputType="password"
+        onInputChange={setPassword}
+      />
+      <Button
+        disabled={email.length === 0 || password.length === 0}
+        onClick={onSignIn}
+      >
+        {loading && <Loader />} Sign In
+      </Button>
       <p className="mt-2 text-center">
-        Create a new account <Link onClick={props.toggleCard}>Sign Up</Link>
+        New here? <Link onClick={props.toggleCard}>Sign Up</Link>
       </p>
     </div>
   );
@@ -16,7 +66,11 @@ export const SignInCard = function (props: any) {
 
 const Button = function (props: any) {
   return (
-    <button className="w-full py-2 mt-2 font-semibold text-white duration-200 bg-purple-500 rounded hover:bg-purple-600 focus:outline-none active:bg-purple-700">
+    <button
+      disabled={props.disabled}
+      className="inline-flex items-center justify-center w-full py-2 mt-2 font-semibold text-white duration-200 bg-purple-500 rounded hover:bg-purple-600 focus:outline-none active:bg-purple-700 disabled:opacity-50"
+      {...props}
+    >
       {props.children}
     </button>
   );
@@ -27,5 +81,13 @@ const Link = function (props: any) {
     <span className="text-blue-700 cursor-pointer hover:underline" {...props}>
       {props.children}
     </span>
+  );
+};
+
+const Alert = function (props: any) {
+  return (
+    <p className="w-full p-1 text-sm font-medium text-center text-white bg-red-500 rounded">
+      {props.children}
+    </p>
   );
 };
