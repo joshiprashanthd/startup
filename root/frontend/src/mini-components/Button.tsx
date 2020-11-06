b57a86df961de47e20112ff8843f74bc3ce7cacc
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-06 18:31:38 +0530

    STYLE: Ad hover and active only if button is not disabled

--- root/frontend/src/mini-components/Button.tsx
+++ root/frontend/src/mini-components/Button.tsx
@@ -5,7 +5,11 @@ export const Button: React.FC<ButtonHTMLAttributes<
 >> = function (props) {
   return (
     <button
-      className="inline-flex items-center justify-center w-full py-2 mt-2 font-semibold text-white duration-200 bg-purple-500 rounded hover:bg-purple-600 focus:outline-none active:bg-purple-700 disabled:opacity-50"
+      className={`inline-flex items-center justify-center w-full py-2 mt-2 font-semibold text-white duration-200 bg-purple-500 rounded shadow-sm ${
+        !props.disabled && "hover:bg-purple-600 "
+      } focus:outline-none ${
+        !props.disabled && "active:bg-purple-700"
+      } disabled:opacity-50`}
       {...props}
     >
       {props.children}
