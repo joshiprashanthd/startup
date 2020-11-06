d6cdff4603146f4d96e59c76473e7b82854532a0
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-06 11:20:52 +0530

    CHORE: Change value length logic

--- root/frontend/src/helpers/validators.ts
+++ root/frontend/src/helpers/validators.ts
@@ -1,18 +1,18 @@
 export const validateEmail = function (value: string) {
   return (
     value.match(/^(?:(?:[\w\.\-_]+@[\w\d]+(?:\.[\w]{2,6})+)[,;]?\s?)+$/) !==
-      null && value.length > 4
+      null && value.length > 0
   );
 };
 
 export const validateHandler = function (value: string) {
-  return value.match(/^\w+$/) !== null && value.length > 3;
+  return value.match(/^\w+$/) !== null && value.length > 0;
 };
 
 export const validateName = function (value: string) {
   return (
     value.match(/^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/) &&
-    value.length > 4
+    value.length > 0
   );
 };
 
