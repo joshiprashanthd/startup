abec1a1662cd5635804f501befc744e1a4522716
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-06 16:28:09 +0530

    CHORE: Remove margin around Loader

--- root/frontend/src/components/Loader.tsx
+++ root/frontend/src/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Loader = function () {
   return (
     <svg
-      className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+      className="w-5 h-5 text-white animate-spin"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
