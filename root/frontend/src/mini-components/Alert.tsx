fea558d33e8fd8166169cec652199aa183fa6843
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-06 21:11:27 +0530

    CHORE: Add different variants to Alert component
    Now Alert component supports "success", "warning", "info", "error" variants.

--- root/frontend/src/mini-components/Alert.tsx
+++ root/frontend/src/mini-components/Alert.tsx
@@ -1,20 +1,45 @@
 import React from "react";
-import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheckCircle,
+  faExclamation,
+  faExclamationCircle,
+  faExclamationTriangle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const Alert = function (props: any) {
+interface IProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  variant?: "error" | "info" | "warning" | "success";
+}
+
+export const Alert: React.FC<IProps> = function ({
+  variant = "info",
+  children,
+  ...restProps
+}) {
+  let variantColor = "blue";
+  let variantIcon = faExclamationCircle;
+
+  switch (variant) {
+    case "error":
+      variantColor = "red";
+      variantIcon = faExclamationTriangle;
+      break;
+    case "warning":
+      variantColor = "orange";
+      variantIcon = faExclamationCircle;
+      break;
+    case "success":
+      variantColor = "green";
+      variantIcon = faCheckCircle;
+      break;
+  }
   return (
     <p
-      className="w-full px-2 py-1 text-sm font-medium text-white bg-red-500 rounded"
-      {...props}
+      className={`w-full px-2 py-1 text-sm font-medium text-white bg-${variantColor}-500 rounded`}
+      {...restProps}
     >
-      <FontAwesomeIcon
-        icon={faExclamationTriangle}
-        color="white"
-        className="mr-1"
-      />
-      {"  "}
-      {props.children}
+      <FontAwesomeIcon icon={variantIcon} color="white" className="mr-1" />{" "}
+      {children}
     </p>
   );
 };
