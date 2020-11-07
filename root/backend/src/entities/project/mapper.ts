b6b0d76dc4bbf6d824f15ce8f13bcea69318bb56
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-07 21:35:49 +0530

    CHORE: Add project mapper

---
+++ root/backend/src/entities/project/mapper.ts
@@ -0,0 +1,19 @@
+import { IContext } from "../../types";
+import { mapSkillIds } from "../skill/mapper";
+import { mapUserId, mapUserIds } from "../user/mapper";
+import { IProjectDocument } from "./model";
+import { IProject } from "./typedef/types";
+
+export const mapProject = (
+	project: IProjectDocument,
+	context: IContext
+): IProject => ({
+	id: project.id,
+	details: {
+		...project.details,
+		skillSet: mapSkillIds(project.details.skillSet, context),
+		creator: mapUserId(project.details.creator, context),
+		stars: mapUserIds(project.details.stars, context)
+	},
+	state: project.state
+});
