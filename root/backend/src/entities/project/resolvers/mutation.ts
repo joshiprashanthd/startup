d6dfc146416afa8557ed14d295f4d50bd139978c
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-07 21:36:04 +0530

    CHORE: Add resolver for createUser mutation

---
+++ root/backend/src/entities/project/resolvers/mutation.ts
@@ -0,0 +1,33 @@
+import { IContext } from "../../../types";
+import { mapProject } from "../mapper";
+import { IProjectDocument, Project } from "../model";
+import { IStrictProjectInput } from "../typedef/types";
+
+type DeepPartial<T> = {
+	[K in keyof T]?: Partial<T[K]>;
+};
+
+export default {
+	Mutation: {
+		createProject: async (
+			parent: any,
+			args: { input: IStrictProjectInput },
+			context: IContext,
+			info: any
+		) => {
+			const skillSet = args.input.details.skillSet as any;
+			const input = {
+				details: {
+					...args.input.details,
+					skillSet: skillSet.map(obj => obj.skillId)
+				}
+			};
+
+			const project = await Project.create<DeepPartial<IProjectDocument>>(
+				input
+			);
+
+			return mapProject(project, context);
+		}
+	}
+};
