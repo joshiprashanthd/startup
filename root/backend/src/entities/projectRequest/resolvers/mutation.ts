856d93da96476f3289655090b7a198b3ab6afc99
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-08 20:18:10 +0530

    CHORE: Add resolver for acceptProjectRequest mutation

--- root/backend/src/entities/projectRequest/resolvers/mutation.ts
+++ root/backend/src/entities/projectRequest/resolvers/mutation.ts
@@ -1,8 +1,13 @@
 //local
+import { AuthenticationError } from "apollo-server-express";
 import { IContext } from "../../../types";
+import { Project } from "../../project/model";
 import { mapProjectRequest } from "../mapper";
 import { ProjectRequest } from "../model";
-import { IProjectRequestDocument } from "../model/types";
+import {
+	IProjectRequestDocument,
+	ProjectRequestStatusEnum
+} from "../model/types";
 
 export default {
 	Mutation: {
@@ -18,10 +23,40 @@ export default {
 				from: context.req.session.userId,
 				to: args.projectId,
 				message: args.message,
-				status: "PENDING"
+				status: ProjectRequestStatusEnum.PENDING
 			});
 
 			return mapProjectRequest(projectRequest, context);
+		},
+
+		acceptProjectRequest: async (
+			parent: any,
+			args: { projectRequestId: string },
+			context: IContext,
+			info: any
+		) => {
+			const projectRequest = await ProjectRequest.findById(
+				args.projectRequestId
+			);
+			const project = await Project.findById(projectRequest.to);
+
+			if (
+				project.details.creator.toString() !==
+				context.req.session.userId.toString()
+			)
+				throw new AuthenticationError(
+					"Your are not allowed to accept this request."
+				);
+
+			const result = await ProjectRequest.findByIdAndUpdate(
+				projectRequest.id,
+				{
+					status: ProjectRequestStatusEnum.ACCEPTED
+				},
+				{ new: true }
+			);
+
+			return mapProjectRequest(result, context);
 		}
 	}
 };
