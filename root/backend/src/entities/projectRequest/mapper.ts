fc994f24bb5db076ca2015ba7ce07e64a85600a1
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-08 13:26:20 +0530

    CHORE: Add ProjectRequest Mapper

---
+++ root/backend/src/entities/projectRequest/mapper.ts
@@ -0,0 +1,15 @@
+import { IContext } from "../../types";
+import { Project } from "../project/model";
+import { mapUserId } from "../user/mapper";
+import { IProjectRequestDocument } from "./model/types";
+
+export const mapProjectRequest = (
+	projectRequest: IProjectRequestDocument,
+	context: IContext
+) => ({
+	id: projectRequest.id,
+	from: mapUserId(projectRequest.from, context),
+	to: async () => Project.findById(projectRequest.to),
+	message: projectRequest.message,
+	status: projectRequest.status
+});
