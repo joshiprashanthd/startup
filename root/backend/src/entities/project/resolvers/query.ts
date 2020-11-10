e77807c6acf54101883b6360b31de491240c2d6e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-10 13:10:45 +0530

    CHORE: Add resolver for projectsByUserInterests query

--- root/backend/src/entities/project/resolvers/query.ts
+++ root/backend/src/entities/project/resolvers/query.ts
@@ -1,10 +1,24 @@
 import { IContext } from "../../../types";
+import { User } from "../../user/model";
 import { mapProject } from "../mapper";
 import { Project } from "../model";
 
 export default {
 	Query: {
 		projects: async (parent: any, args: any, context: IContext, info: any) =>
-			(await Project.find({})).map(project => mapProject(project, context))
+			(await Project.find({})).map(project => mapProject(project, context)),
+		projectsByUserInterests: async (
+			parent: any,
+			args: { userId: string },
+			context: IContext,
+			info: any
+		) => {
+			const user = await User.findById(args.userId);
+			const skillIds = user.personalInfo.interests;
+			console.log(skillIds);
+			return (
+				await Project.find({ "details.skillSet": { $in: skillIds } })
+			).map(project => mapProject(project, context));
+		}
 	}
 };
