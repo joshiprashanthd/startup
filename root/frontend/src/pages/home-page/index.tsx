25dd444564b30e9a3c30498d291d4feb19f90da0
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-10 15:27:08 +0530

    CHORE: Add NavBar to HomePage

--- root/frontend/src/pages/home-page/index.tsx
+++ root/frontend/src/pages/home-page/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Navbar } from "../../components/navbar";
 
 export const HomePage = function (props: any) {
   return (
     <div className="w-full min-h-screen">
-      <h1 className="text-4xl font-display"> The home page</h1>
+      <Navbar selected="explore" />
+      <h1 className="w-full h-screen text-4xl font-display"> The home page</h1>
     </div>
   );
 };
