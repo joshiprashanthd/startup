006e4562c18bbea4e4d599727741c0b9f61c6121
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-10 22:14:12 +0530

    CHORE: Add Dropdown component

---
+++ root/frontend/src/components/core/dropdown.tsx
@@ -0,0 +1,62 @@
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { createContext, useContext, useState } from "react";
+
+const DropdownContext = createContext({
+  onSelected: (value: any) => {},
+  showDropdown: (value: any) => {},
+});
+
+export const Dropdown = function (props: any) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <DropdownContext.Provider
+      value={{ onSelected: props.onSelected, showDropdown: setShow }}
+    >
+      <div className="relative inline-block">
+        <div>
+          <button
+            onClick={() => setShow((prev) => !prev)}
+            className="p-2 text-sm font-medium transition duration-200 ease-in-out border rounded shadow-sm focus:outline-none focus:bg-gray-100"
+          >
+            {props.label}
+            <FontAwesomeIcon icon={faAngleDown} className="ml-4" />
+          </button>
+        </div>
+        {show && props.children}
+      </div>
+    </DropdownContext.Provider>
+  );
+};
+
+Dropdown.Menu = function (props: any) {
+  return (
+    <div className="absolute right-0 min-w-full mt-2 overflow-hidden text-sm origin-top-right bg-white rounded-md shadow-lg font-body">
+      {props.children}
+    </div>
+  );
+};
+
+Dropdown.ItemHeader = function (props: any) {
+  return (
+    <span className="block p-2 text-xs font-semibold text-gray-800">
+      {props.children}
+    </span>
+  );
+};
+
+Dropdown.Item = function (props: any) {
+  const dropdownContext = useContext(DropdownContext);
+  return (
+    <p
+      className="block min-w-full p-2 text-sm cursor-pointer hover:bg-gray-300"
+      onClick={() => {
+        dropdownContext.onSelected(props.value);
+        dropdownContext.showDropdown(false);
+      }}
+    >
+      {props.children}
+    </p>
+  );
+};
