c389946855d8a32315c47a646ae9144b8d1df4e2
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-10 15:33:40 +0530

    FIX: Change navbar selected prop to "create"

--- root/frontend/src/pages/create-page/index.tsx
+++ root/frontend/src/pages/create-page/index.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "../../components/navbar";
 export const CreatePage = function (props: any) {
   return (
     <div className="w-full min-h-screen">
-      <Navbar selected="explore" />
+      <Navbar selected="create" />
       <h1 className="w-full h-screen text-4xl font-display">The create page</h1>
     </div>
   );
