a52afbb69134285b6173fea2ce50e2bb89feb7c4
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-10 16:31:10 +0530

    CHORE: Add CreatePage link in navbar

--- root/frontend/src/components/navbar.tsx
+++ root/frontend/src/components/navbar.tsx
@@ -5,20 +5,33 @@ export const Navbar = function (props: any) {
   return (
     <div className="flex flex-row w-full h-12 px-6 py-0 bg-white shadow-lg">
       <div className="flex flex-row w-48 mx-auto">
-        <div
-          className={`w-24 px-2 py-3 font-bold text-center ${
-            props.selected === "explore" && "text-blue-700 bg-blue-200"
-          } border-l border-r font-body`}
-        >
-          <Link to="/home">Explore</Link>
-        </div>
-        <div
-          className={`w-24 px-2 py-3 font-bold text-center ${
-            props.selected === "issues" && "text-blue-700 bg-blue-200"
-          } border-l border-r font-body`}
-        >
-          <Link to="/issues">Issues</Link>
-        </div>
+        <Link to="/home">
+          <div
+            className={`w-24 px-2 py-3 font-bold text-center ${
+              props.selected === "explore" && "text-purple-700 bg-purple-200"
+            } border-l border-r font-body`}
+          >
+            Explore
+          </div>
+        </Link>
+        <Link to="/issues">
+          <div
+            className={`w-24 px-2 py-3 font-bold text-center ${
+              props.selected === "issues" && "text-purple-700 bg-purple-200"
+            } border-l border-r font-body`}
+          >
+            Issues
+          </div>
+        </Link>
+        <Link to="/create">
+          <div
+            className={`w-24 px-2 py-3 font-bold text-center ${
+              props.selected === "create" && "text-purple-700 bg-purple-200"
+            } border-l border-r font-body`}
+          >
+            Create
+          </div>
+        </Link>
       </div>
     </div>
   );
