db88eead735372c1f74ccc082ef5cdba6098ee5e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-11 14:54:48 +0530

    CHORE: Add types to Chip component

--- root/frontend/src/components/core/chip.tsx
+++ root/frontend/src/components/core/chip.tsx
@@ -2,19 +2,28 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-export const Chip = function (props: any) {
+interface IProps extends React.HTMLAttributes<HTMLDivElement> {
+  color?: string;
+  value: string;
+  onDelete: (value: string) => void;
+}
+
+export const Chip: React.FC<IProps> = function ({
+  color = "purple",
+  onDelete = (value: string) => {},
+  value,
+  children,
+}) {
   return (
     <div
-      className={`flex items-center mx-1 font-medium text-sm py-1 px-2 rounded text-white bg-${
-        props.color || "purple"
-      }-500`}
+      className={`flex items-center mx-1 font-medium text-sm py-1 px-2 rounded text-white bg-${color}-500`}
     >
-      {props.children}
+      {children}
       <FontAwesomeIcon
         icon={faTimes}
         color="white"
         className="ml-2 cursor-pointer"
-        onClick={() => props.onDelete(props.value)}
+        onClick={() => onDelete(value)}
       />
     </div>
   );
