83465b6725e53ecd9f3b116e870a2a808896faa0
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-12 15:10:43 +0530

    CHORE: Add ProjectPostCard component

--- root/frontend/src/pages/home-page/index.tsx
+++ root/frontend/src/pages/home-page/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navbar } from "../../components/navbar";
+import { ProjectPostCard } from "../../components/project-post-card";
 
 export const HomePage = function (props: any) {
   return (
     <div className="w-full min-h-screen">
       <Navbar selected="explore" />
-      <h1 className="w-full h-screen text-4xl font-display"> The home page</h1>
+      <div className="w-2/4 min-h-screen py-16 mx-auto">
+        <ProjectPostCard />
+      </div>
     </div>
   );
 };
