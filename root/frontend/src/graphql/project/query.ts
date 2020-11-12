93c40e85dd546bd9214bb6e95085ded37a8b9e7e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-12 18:49:06 +0530

    CHORE: Add constant for all projects query

---
+++ root/frontend/src/graphql/project/query.ts
@@ -0,0 +1,24 @@
+import { gql } from "@apollo/client";
+
+export const PROJECTS = gql`
+  {
+    projects {
+      id
+      details {
+        title
+        description
+        duration
+        startingOn
+        skillSet {
+          name
+        }
+        creator {
+          accountInfo {
+            name
+            handler
+          }
+        }
+      }
+    }
+  }
+`;
