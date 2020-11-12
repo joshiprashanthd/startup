31660703f0ab6d35d8ef4a72bb0e33ab1abaf76d
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-12 18:50:56 +0530

    FEAT:  Use projects query to generate ProjectPostCards list on home page

--- root/frontend/src/pages/home-page/index.tsx
+++ root/frontend/src/pages/home-page/index.tsx
@@ -1,13 +1,22 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { Loader } from "../../components/core/loader";
 import { Navbar } from "../../components/navbar";
-import { ProjectPostCard } from "../../components/project-post-card";
+import ProjectPostCard from "../../components/project-post-card";
+import { PROJECTS } from "../../graphql/project/query";
 
 export const HomePage = function (props: any) {
+  const { data, loading, error } = useQuery(PROJECTS);
+
   return (
     <div className="w-full min-h-screen">
       <Navbar selected="explore" />
       <div className="w-2/4 min-h-screen py-16 mx-auto">
-        <ProjectPostCard />
+        {loading && <Loader />}
+        {data &&
+          data.projects.map((project: any) => (
+            <ProjectPostCard project={project} key={project.id} />
+          ))}
       </div>
     </div>
   );
