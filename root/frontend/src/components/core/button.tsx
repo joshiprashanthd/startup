a1fdfab83bdc0ec7e81c257d46e635b43165b305
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-13 15:23:38 +0530

    CHORE: Add variant prop to Button component
    variant prop can used to set the primary or secondary theme of the button.

--- root/frontend/src/components/core/button.tsx
+++ root/frontend/src/components/core/button.tsx
@@ -1,18 +1,31 @@
 import React, { ButtonHTMLAttributes } from "react";
 
-export const Button: React.FC<ButtonHTMLAttributes<
-  HTMLButtonElement
->> = function (props) {
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary";
+}
+
+export const Button: React.FC<IProps> = function ({
+  children,
+  variant = "primary",
+  disabled,
+  ...restProps
+}) {
+  let className = `w-full inline-flex items-center justify-center p-2 text-sm font-medium text-white duration-200 bg-purple-500 rounded shadow-sm  outline-none ${
+    !disabled && "hover:bg-purple-600 "
+  } focus:outline-none ${
+    !disabled && "active:bg-purple-700"
+  } disabled:opacity-50 font-body `;
+
+  if (variant === "secondary")
+    className = `w-full inline-flex items-center justify-center text-sm p-2 font-medium duration-200 bg-white border rounded shadow-sm outline-none ${
+      !disabled && "hover:bg-gray-200 "
+    } focus:outline-none ${
+      !disabled && "active:bg-gray-300"
+    } disabled:opacity-50 font-body `;
+
   return (
-    <button
-      className={`inline-flex items-center justify-center w-full py-2 mt-2 font-semibold text-white duration-200 bg-purple-500 rounded shadow-sm ${
-        !props.disabled && "hover:bg-purple-600 "
-      } focus:outline-none ${
-        !props.disabled && "active:bg-purple-700"
-      } disabled:opacity-50 font-body`}
-      {...props}
-    >
-      {props.children}
+    <button className={className} disabled={disabled} {...restProps}>
+      {children}
     </button>
   );
 };
