9b7f40caf7082542601e4e0661dd428900071fd7
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-13 21:32:20 +0530

    CHORE: Change request project to toggleRequestProject

--- root/frontend/src/graphql/projectRequest/mutation.ts
+++ root/frontend/src/graphql/projectRequest/mutation.ts
@@ -1,9 +1,7 @@
 import { gql } from "@apollo/client";
 
-export const REQUEST_PROJECT = gql`
-  mutation RequestProject($message: String!, $projectId: ID!) {
-    requestProject(message: $message, projectId: $projectId) {
-      id
-    }
+export const TOGGLE_REQUEST_PROJECT = gql`
+  mutation ToggleRequestProject($message: String!, $projectId: ID!) {
+    toggleRequestProject(message: $message, projectId: $projectId)
   }
 `;
