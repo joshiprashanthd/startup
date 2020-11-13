e7d1c7a77d13fd357d60d59c3b05d0e92118e609
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-13 21:56:19 +0530

    CHORE: Store isRequested in local state requested variable

--- root/frontend/src/components/project-post-card/index.tsx
+++ root/frontend/src/components/project-post-card/index.tsx
@@ -3,29 +3,53 @@ import React, { useState } from "react";
 
 //local
 import { TOGGLE_STAR_PROJECT } from "../../graphql/project/mutation";
+import { TOGGLE_REQUEST_PROJECT } from "../../graphql/projectRequest/mutation";
 import { ProjectPostCard } from "./project-post-card";
 
 export default function (props: any) {
   const { project } = props;
 
   const [starred, setStarred] = useState(project.isStarred);
+  const [requested, setRequested] = useState(project.isRequested);
+
+  const [toggleRequestProject] = useMutation(TOGGLE_REQUEST_PROJECT, {
+    onCompleted: (data) => {
+      console.log(data);
+      setRequested(data.toggleRequestProject);
+    },
+    onError: (err) => console.log(err),
+  });
+
   const [toggleStarProject] = useMutation(TOGGLE_STAR_PROJECT, {
     onCompleted: (data) => {
       setStarred(data.toggleStarProject);
     },
   });
 
-  const toggleStarCallback = async () => {
+  const toggleStarCallback = () => {
     toggleStarProject({
       variables: {
         projectId: project.id,
       },
     });
   };
+
+  const toggleRequestCallback = (message: string) => {
+    toggleRequestProject({
+      variables: {
+        message,
+        projectId: project.id,
+      },
+    });
+  };
   return (
     <ProjectPostCard
-      project={Object.assign({ ...project }, { isStarred: starred })}
+      project={Object.assign(
+        { ...project },
+        { isStarred: starred, isRequested: requested }
+      )}
       toggleStarCallback={toggleStarCallback}
+      toggleRequestCallback={toggleRequestCallback}
     />
   );
 }
