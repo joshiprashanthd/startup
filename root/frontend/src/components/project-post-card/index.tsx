73291158082632fac56b2dea7d95cc9268b4afd6
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-13 13:33:33 +0530

    CHORE: Add star button logic
    starred is local state to which assigns predicted isStarred value to project object

--- root/frontend/src/components/project-post-card/index.tsx
+++ root/frontend/src/components/project-post-card/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
 
 //local
+import { TOGGLE_STAR_PROJECT } from "../../graphql/project/mutation";
 import { ProjectPostCard } from "./project-post-card";
 
 export default function (props: any) {
   const { project } = props;
-  return <ProjectPostCard project={project} />;
+
+  const [starred, setStarred] = useState(project.isStarred);
+  const [toggleStarProject] = useMutation(TOGGLE_STAR_PROJECT, {
+    onCompleted: (data) => {
+      setStarred(data.toggleStarProject);
+    },
+  });
+
+  const toggleStarCallback = async () => {
+    toggleStarProject({
+      variables: {
+        projectId: project.id,
+      },
+    });
+  };
+  return (
+    <ProjectPostCard
+      project={Object.assign({ ...project }, { isStarred: starred })}
+      toggleStarCallback={toggleStarCallback}
+    />
+  );
 }
