0c174785844fb389df886eae5925e50c0edf7c02
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-15 00:12:49 +0530

    STYLE: Add hover style to secondary variant of Button

--- root/frontend/src/components/core/button.tsx
+++ root/frontend/src/components/core/button.tsx
@@ -10,7 +10,7 @@ let ButtonStyles = {
     "w-full inline-flex items-center justify-center text-sm font-medium font-body focus:outline-none rounded outline-none",
   primary:
     "text-white duration-200 bg-purple-700 hover:bg-purple-800 disabled:bg-gray-300 p-2",
-  secondary: "bg-white border disabled:bg-gray-300 p-2",
+  secondary: "bg-white border hover:bg-gray-100 disabled:bg-gray-300 p-2",
   flat: "text-black bg-white disabled:text-gray-200",
 };
 
