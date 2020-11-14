f6ff76fd17c292a93761e0ba9ac195737e08a42e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-14 15:32:29 +0530

    CHORE: Add dropdown for user options

--- root/frontend/src/components/navbar.tsx
+++ root/frontend/src/components/navbar.tsx
@@ -1,9 +1,15 @@
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+//local
+import AuthContext, { IAuthContext, IAuthInfo } from "../contexts/auth-context";
+import { Dropdown } from "./core/dropdown";
+
 export const Navbar = function (props: any) {
+  const authContext = useContext(AuthContext);
+
   return (
     <div className="flex flex-row w-full h-12 px-6 py-0 bg-white shadow-sm">
       <div className="flex-1"></div>
@@ -38,13 +44,20 @@ export const Navbar = function (props: any) {
       </div>
       <div className="flex items-center justify-end flex-1">
         <div className="grid w-8 h-8 mr-4 text-base font-semibold text-black bg-purple-300 rounded place-items-center">
-          JJ
+          {(authContext.user?.name?.split(" ")[0].charAt(0) as string) +
+            authContext.user?.name?.split(" ")[1].charAt(0)}
         </div>
         <div className="mr-4">
-          <span className="font-medium">Jastor J</span>
+          <span className="font-medium">{authContext.user?.name}</span>
         </div>
         <div>
-          <FontAwesomeIcon icon={faAngleDown} />
+          <Dropdown icon={<FontAwesomeIcon icon={faAngleDown} />}>
+            <Dropdown.Menu width="48">
+              <Dropdown.ItemHeader>Your</Dropdown.ItemHeader>
+              <Dropdown.Item>Your Profile</Dropdown.Item>
+              <Dropdown.Item>Your Projects</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
       </div>
     </div>
