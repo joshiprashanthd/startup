1d75db92c896c76770c0154219f45df4e53e0605
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-14 14:10:18 +0530

    CHORE: Add user information in the navbar
    This is just a mockup for now.

--- root/frontend/src/components/navbar.tsx
+++ root/frontend/src/components/navbar.tsx
@@ -1,10 +1,13 @@
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = function (props: any) {
   return (
     <div className="flex flex-row w-full h-12 px-6 py-0 bg-white shadow-sm">
-      <div className="flex flex-row w-48 mx-auto">
+      <div className="flex-1"></div>
+      <div className="flex flex-row justify-center flex-1 w-auto">
         <Link to="/home">
           <div
             className={`w-24 px-2 py-3 font-bold text-center ${
@@ -32,6 +35,17 @@ export const Navbar = function (props: any) {
             Create
           </div>
         </Link>
+      </div>
+      <div className="flex items-center justify-end flex-1">
+        <div className="grid w-8 h-8 mr-4 text-base font-semibold text-black bg-purple-300 rounded place-items-center">
+          JJ
+        </div>
+        <div className="mr-4">
+          <span className="font-medium">Jastor J</span>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faAngleDown} />
+        </div>
       </div>
     </div>
   );
