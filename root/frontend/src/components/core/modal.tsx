179161519acd65567fa2171f789d468a639b4231
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-14 13:55:20 +0530

    STYLE: Add overflow-y-auto to modal div element

--- root/frontend/src/components/core/modal.tsx
+++ root/frontend/src/components/core/modal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Modal = function (props: any) {
   return (
-    <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full">
+    <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full overflow-y-auto">
       <div className="absolute w-full h-full bg-gray-600 opacity-50 pointer-events-none"></div>
       <div className="z-50 w-3/6 p-4 bg-white rounded-lg">{props.children}</div>
     </div>
