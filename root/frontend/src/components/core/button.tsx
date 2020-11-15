17dd51b25f65c4af3bda6b7ec207fb940d23a758
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-15 13:41:17 +0530

    STYLE: Change disabled button style

--- root/frontend/src/components/core/button.tsx
+++ root/frontend/src/components/core/button.tsx
@@ -9,9 +9,10 @@ let ButtonStyles = {
   base:
     "w-full inline-flex items-center justify-center text-sm font-medium font-body focus:outline-none rounded outline-none",
   primary:
-    "text-white duration-200 bg-purple-700 hover:bg-purple-800 disabled:bg-gray-300 p-2",
-  secondary: "bg-white border hover:bg-gray-100 disabled:bg-gray-300 p-2",
-  flat: "text-black bg-white disabled:text-gray-200",
+    "text-white duration-200 bg-purple-700 hover:bg-purple-800 disabled:bg-gray-300 disabled:text-gray-500 p-2",
+  secondary:
+    "bg-white border hover:bg-gray-100 disabled:bg-gray-300 disabled:text-gray-600 p-2",
+  flat: "text-black bg-white disabled:text-gray-300",
 };
 
 export const Button: React.FC<IProps> = function ({
