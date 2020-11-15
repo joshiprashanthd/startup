61d7e68d2400d891ff14a41bf62c36b5f52674c6
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-15 19:31:16 +0530

    CHORE: Add new props to SizedBox

--- root/frontend/src/components/core/sized-box.tsx
+++ root/frontend/src/components/core/sized-box.tsx
@@ -39,12 +39,99 @@ interface IProps {
     | 48
     | 56
     | 64;
+  minWidth?:
+    | "auto"
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 8
+    | 10
+    | 12
+    | 16
+    | 20
+    | 24
+    | 32
+    | 40
+    | 48
+    | 56
+    | 64;
+
+  minHeight?:
+    | "auto"
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 8
+    | 10
+    | 12
+    | 16
+    | 20
+    | 24
+    | 32
+    | 40
+    | 48
+    | 56
+    | 64;
+  maxWidth?:
+    | "auto"
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 8
+    | 10
+    | 12
+    | 16
+    | 20
+    | 24
+    | 32
+    | 40
+    | 48
+    | 56
+    | 64;
+  maxHeight?:
+    | "auto"
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 8
+    | 10
+    | 12
+    | 16
+    | 20
+    | 24
+    | 32
+    | 40
+    | 48
+    | 56
+    | 64;
 }
 
 export const SizedBox: React.FC<IProps> = function ({
   children,
   width = "auto",
   height = "auto",
+  minWidth = "auto",
+  minHeight = "auto",
+  maxWidth = "auto",
+  maxHeight = "auto",
 }) {
-  return <div className={`w-${width} h-${height}`}>{children}</div>;
+  return (
+    <div
+      className={`w-${width} h-${height} min-w-${minWidth} min-h-${minHeight} max-w-${maxWidth} max-h-${maxHeight}`}
+    >
+      {children}
+    </div>
+  );
 };
