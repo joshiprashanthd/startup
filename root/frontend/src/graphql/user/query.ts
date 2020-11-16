9e2eabb67144dfb4defb7075e824618dfe96497e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-16 20:38:59 +0530

    CHORE: Add me query for profile page

--- root/frontend/src/graphql/user/query.ts
+++ root/frontend/src/graphql/user/query.ts
@@ -12,3 +12,23 @@ export const ME = gql`
     }
   }
 `;
+
+export const ME_PROFILE_PAGE = gql`
+  {
+    me {
+      id
+      accountInfo {
+        name
+        email
+        handler
+      }
+      personalInfo {
+        bio
+        birthDate
+        interests {
+          name
+        }
+      }
+    }
+  }
+`;
