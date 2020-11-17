9041937d8fe836c88c0a77cb10bb3cdfc27efd1a
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-17 12:51:23 +0530

    STYLE: Add border and reduce shadow of project post

--- root/frontend/src/components/project-post-card/project-post-card.tsx
+++ root/frontend/src/components/project-post-card/project-post-card.tsx
@@ -23,7 +23,7 @@ export const ProjectPostCard = function (props: any) {
     .join(", ");
 
   return (
-    <div className="w-4/5 p-4 my-4 rounded shadow-md">
+    <div className="w-4/5 p-4 my-4 bg-white border rounded shadow">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <Avatar name={avatarName} />
