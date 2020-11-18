21fa6917a08225c33751ecff9c5129cb1a66a718
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-18 17:38:11 +0530

    CHORE: aAdd InputFieldWithDropdown component
    This component can be used to generate suggestion as you type.
    
    InputFieldWithDropdown component require onChange and value props.
    
    This props works exactly like <input> element since they are directly passed to underlying <input> element.

---
+++ root/frontend/src/components/input-field-with-dropdown.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+interface IProps {
+  onChange: (value: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  inputPlaceholder?: string;
+}
+
+export const InputFieldWithDropdown: React.FC<IProps> & {
+  Dropdown: React.FC<any>;
+  DropdownItem: React.FC<any>;
+} = function ({
+  children = null,
+  onChange,
+  value,
+  inputPlaceholder = "Enter text",
+}) {
+  return (
+    <div className="relative w-full text-left">
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        className="w-full p-2 text-sm placeholder-gray-400 duration-200 rounded outline-none focus:outline-none font-body"
+        placeholder={inputPlaceholder}
+      />
+      {value.length > 0 && children}
+    </div>
+  );
+};
+
+const Dropdown: React.FC = function ({ children }) {
+  return (
+    <div className="absolute object-left-top w-56 py-1 mt-2 mb-4 overflow-y-auto text-sm bg-white rounded shadow-lg max-h-48 font-body">
+      {children}
+    </div>
+  );
+};
+
+interface IDropdownItemProps {
+  onSelect: (value: any) => void;
+  value: any;
+}
+
+const DropdownItem: React.FC<IDropdownItemProps> = function ({
+  children,
+  onSelect,
+  value,
+}) {
+  return (
+    <div
+      className="w-full p-2 text-sm bg-white cursor-pointer group hover:bg-purple-700 font-body"
+      onClick={() => onSelect(value)}
+    >
+      {children}
+    </div>
+  );
+};
+
+InputFieldWithDropdown.Dropdown = Dropdown;
+InputFieldWithDropdown.DropdownItem = DropdownItem;
