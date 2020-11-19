ffbf0bde6735d201a49429c757bbb1ae42445bc9
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-19 17:53:13 +0530

    CHORE: Add working skill seletor to ProfilePage
    The profile page is not completed yet.
    
    This commit is backup of the working state of profile page.

--- root/frontend/src/pages/profile-page/index.tsx
+++ root/frontend/src/pages/profile-page/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import fuse from "fuse.js";
 
 //local
 import { Page } from "../../components/core/page";
@@ -14,50 +15,125 @@ import { Anchor } from "../../components/core/anchor";
 import { Button } from "../../components/core/button";
 import { SizedBox } from "../../components/core/sized-box";
 import { InputField } from "../../components/core/input-field";
+import { DatePicker } from "../../components/core/date-picker";
 
 export const ProfilePage = function (props: any) {
-  const [edit, setEdit] = useState(false);
-  const { data, loading, error } = useQuery(ME_PROFILE_PAGE);
+  const { data, loading } = useQuery(ME_PROFILE_PAGE);
   return (
-    <>
+    <Page>
       {data && (
-        <Page>
+        <>
           <Navbar />
           <div className="flex w-4/5 mx-auto">
             <LeftPane data={data} />
-            <RightPane data={data} edit={edit} setEdit={setEdit} />
+            <RightPane data={data} />
           </div>
-        </Page>
+        </>
       )}
-    </>
+    </Page>
   );
 };
 
-const BioEditor = function (props: any) {
+const Bio = function (props: any) {
   const { edit, data } = props;
   return (
-    <>
-      <h1 className="text-sm font-medium text-gray-700">Bio</h1>
-      {!edit && <span>{data.me.personalInfo.bio || "Add a bio"}</span>}
-      {edit && <InputField textareaMode />}
-    </>
+    <div className="mb-2">
+      <h1 className="text-sm font-medium text-gray-700 font-body">Bio</h1>
+      {edit ? (
+        <SizedBox width={56}>
+          <InputField />
+        </SizedBox>
+      ) : (
+        <span className="font-body">
+          {data || "Add something about you..."}
+        </span>
+      )}
+    </div>
   );
 };
 
-const SkillsEditor = function (props: any) {
+const Name = function (props: any) {
+  const { edit, data } = props;
+  return (
+    <div className="mb-2">
+      <h1 className="text-sm font-medium text-gray-700 font-body">Name</h1>
+      {edit ? (
+        <SizedBox width={56}>
+          <InputField />
+        </SizedBox>
+      ) : (
+        <span className="font-body">{data}</span>
+      )}
+    </div>
+  );
+};
+
+const Email = function (props: any) {
+  const { edit, data } = props;
+  return (
+    <div className="mb-2">
+      <h1 className="text-sm font-medium text-gray-700 font-body">Email</h1>
+      {edit ? (
+        <SizedBox width={56}>
+          <InputField />
+        </SizedBox>
+      ) : (
+        <span className="font-body">{data}</span>
+      )}
+    </div>
+  );
+};
+
+const Handler = function (props: any) {
+  const { edit, data } = props;
+
+  return (
+    <div className="mb-2">
+      <h1 className="text-sm font-medium text-gray-700 font-body">Handler</h1>
+      {edit ? (
+        <SizedBox width={56}>
+          <InputField />
+        </SizedBox>
+      ) : (
+        <span className="font-body">{data}</span>
+      )}
+    </div>
+  );
+};
+
+const BirthDate = function (props: any) {
+  const { edit, data } = props;
+
+  return (
+    <div className="mb-2">
+      <h1 className="text-sm font-medium text-gray-700 font-body">
+        Birth date
+      </h1>
+      {edit ? (
+        <SizedBox width={64}>
+          <DatePicker onSelectDate={props.set} />
+        </SizedBox>
+      ) : (
+        <span className="font-body">{data || "Not given"}</span>
+      )}
+    </div>
+  );
+};
+
+const Interests = function (props: any) {
   const { data, loading } = useQuery(SKILLS_WITH_NAME_ID);
   const [selected, setSelected] = useState<{ name: string; id: string }[]>(
-    props.data.me.personalInfo.interests.map(
-      (skill: { name: string; id: string }) => ({
-        name: skill.name,
-        id: skill.id,
-      })
-    )
+    props.data.map((skill: { name: string; id: string }) => ({
+      name: skill.name,
+      id: skill.id,
+    }))
   );
   const [value, setValue] = useState("");
 
-  const handleOnDelete = (id: string) =>
+  const handleOnDelete = (id: string) => {
     setSelected((prev) => prev.filter((skill) => skill.id !== id));
+    props.set(selected.map((skill) => skill.id));
+  };
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.currentTarget.value);
@@ -65,18 +141,23 @@ const SkillsEditor = function (props: any) {
 
   const handleOnSelect = (value: { name: string; id: string }) => {
     setSelected((prev) => [...prev, value]);
+    props.set(selected.map((skill) => skill.id));
     setValue("");
   };
 
   return (
-    <div className="mt-4">
-      <h1 className="text-sm font-medium text-gray-700">Skills</h1>
-      {props.edit &&
-        selected.map((skill) => (
-          <Chip onDelete={handleOnDelete} value={skill.id} key={skill.id}>
-            {skill.name}
-          </Chip>
-        ))}
+    <div>
+      <h1 className="mb-1 text-sm font-medium text-gray-700 font-body">
+        Skills
+      </h1>
+      <div className="flex space-x-2">
+        {props.edit &&
+          selected.map((skill) => (
+            <Chip onDelete={handleOnDelete} value={skill.id} key={skill.id}>
+              {skill.name}
+            </Chip>
+          ))}
+      </div>
       {props.edit && (
         <>
           <InputFieldWithDropdown
@@ -87,42 +168,50 @@ const SkillsEditor = function (props: any) {
             <InputFieldWithDropdown.Dropdown>
               {loading && <Loader color="purple-700" />}
               {data &&
-                data.skills.map(
-                  (skill: {
-                    id: string;
-                    name: string;
-                    description: string;
-                  }) => (
-                    <InputFieldWithDropdown.DropdownItem
-                      onSelect={handleOnSelect}
-                      value={{ name: skill.name, id: skill.id }}
-                    >
-                      <span className="text-sm font-medium font-body">
-                        {skill.name}
-                      </span>
-                      <span className="text-xs text-gray-700">
-                        {skill.description}
-                      </span>
-                    </InputFieldWithDropdown.DropdownItem>
-                  )
-                )}
+                new fuse<any>(
+                  data.skills.filter(
+                    (skill: { id: string }) =>
+                      !selected.find((obj) => obj.id === skill.id)
+                  ),
+                  { keys: ["name", "description"] }
+                )
+                  .search(value)
+                  .map(
+                    (skill: {
+                      item: {
+                        id: string;
+                        name: string;
+                        description: string;
+                      };
+                    }) => (
+                      <InputFieldWithDropdown.DropdownItem
+                        onSelect={handleOnSelect}
+                        value={{ name: skill.item.name, id: skill.item.id }}
+                      >
+                        <p className="text-sm font-medium font-body group-hover:text-white">
+                          {skill.item.name}
+                        </p>
+                        <span className="text-xs text-gray-700 group-hover:text-white">
+                          {skill.item.description}
+                        </span>
+                      </InputFieldWithDropdown.DropdownItem>
+                    )
+                  )}
             </InputFieldWithDropdown.Dropdown>
           </InputFieldWithDropdown>
         </>
       )}
       {!props.edit &&
-        props.data.me.personalInfo.interests.map(
-          (skill: { name: string; id: string }) => (
-            <Chip key={skill.name}>{skill.name}</Chip>
-          )
-        )}
+        props.data.map((skill: { name: string; id: string }) => (
+          <Chip key={skill.name}>{skill.name}</Chip>
+        ))}
     </div>
   );
 };
 
 const LeftPane = function (props: any) {
   return (
-    <div className="w-1/5 p-4 text-left border left">
+    <div className="p-4 text-left border left">
       <div className="grid w-40 h-40 text-6xl font-bold text-white uppercase bg-purple-700 rounded-xl place-items-center">
         {extractInitials(props.data.me.accountInfo.name)}
       </div>
@@ -139,17 +228,58 @@ const LeftPane = function (props: any) {
 };
 
 const RightPane = function (props: any) {
-  const { edit, setEdit } = props;
+  const { data } = props;
   return (
     <div className="w-full px-8 py-4 bg-white border right">
+      <PersonalInformationSection data={data} />
+    </div>
+  );
+};
+
+const PersonalInformationSection = function (props: any) {
+  const { data, loading } = props;
+  const [edit, setEdit] = useState(false);
+  const [name, setName] = useState(data.me.accountInfo.name);
+  const [handler, setHandler] = useState(data.me.accountInfo.handler);
+  const [email, setEmail] = useState(data.me.accountInfo.email);
+  const [birthDate, setBirthDate] = useState(data.me.personalInfo.birthDate);
+  const [bio, setBio] = useState(data.me.personalInfo.bio);
+  const [skills, setSkills] = useState<string[]>([]);
+
+  return (
+    <div className="my-4">
       <div className="flex items-center justify-between">
         <h1 className="text-lg font-medium font-body">Personal Information</h1>
         {!edit && (
-          <Anchor fontSize="sm" fontWeight="medium" textColor="gray-700">
+          <Anchor
+            fontSize="sm"
+            fontWeight="medium"
+            textColor="gray-700"
+            onClick={() => setEdit(true)}
+          >
             Edit
           </Anchor>
         )}
       </div>
+
+      <Name data={data.me.accountInfo.name} edit={edit} set={setName} />
+      <Handler
+        data={data.me.accountInfo.handler}
+        edit={edit}
+        set={setHandler}
+      />
+      <Email data={data.me.accountInfo.email} edit={edit} set={setEmail} />
+      <BirthDate
+        data={data.me.personalInfo.birthDate}
+        edit={edit}
+        set={setBirthDate}
+      />
+      <Bio data={data.me.personalInfo.bio} edit={edit} set={setBio} />
+      <Interests
+        data={data.me.personalInfo.interests}
+        edit={edit}
+        set={setSkills}
+      />
 
       {edit && (
         <div className="flex mt-4 space-x-2">
