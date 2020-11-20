f45fcfc090293742f259793a6c5fb95575ab799e
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-20 15:26:24 +0530

    CHORE: Add redirect to home is user is not null

--- root/frontend/src/App.tsx
+++ root/frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import { useQuery } from "@apollo/client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -9,8 +8,6 @@ import {
 
 //local
 import { AuthProvider } from "./components/auth-provider";
-import { IAuthInfo } from "./contexts/auth-context";
-import { ME } from "./graphql/user/query";
 import { useProvideAuth } from "./hooks/useProvideAuth";
 import { AuthPage } from "./pages/auth-page";
 import { CreatePage } from "./pages/create-page";
@@ -19,30 +16,21 @@ import { IssuesPage } from "./pages/issues-page";
 import { ProfilePage } from "./pages/profile-page";
 
 export default function App() {
-  const { data } = useQuery(ME);
   const auth = useProvideAuth();
-
-  useEffect(() => {
-    if (data) {
-      const user: IAuthInfo = {
-        id: data.me.id,
-        email: data.me.accountInfo.email,
-        handler: data.me.accountInfo.handler,
-        name: data.me.accountInfo.name,
-      };
-      auth.signIn(user);
-    }
-  }, [data]);
 
   return (
     <AuthProvider value={auth}>
       <Router>
         <Switch>
           {auth.user === null && <Redirect exact from="/" to="/auth" />}
-          {auth.user && <Redirect exact from="/auth" to="/home" />}
-          <Route path="/home">
-            <HomePage />
-          </Route>
+          {auth.user && !auth.loading && (
+            <Redirect exact from="/auth" to="/home" />
+          )}
+          {!auth.loading && (
+            <Route path="/home">
+              <HomePage />
+            </Route>
+          )}
           <Route path="/profile/:handler">
             <ProfilePage />
           </Route>
