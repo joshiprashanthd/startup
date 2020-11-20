0c2553645e3b65f6362b818c087614bfd4f40d0c
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-20 10:28:33 +0530

    CHORE: Add initialValue prop to InputField
    This `initialValue` prop is directly passed to the internal value state.
    
    This value is bind to the actual `input` element.

--- root/frontend/src/components/core/input-field.tsx
+++ root/frontend/src/components/core/input-field.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 interface IProps {
   label?: string;
@@ -8,6 +8,7 @@ interface IProps {
   errorMessage?: string;
   onInputChange?: (value: string) => void;
   textareaMode?: boolean;
+  initialValue?: string;
 }
 
 export const InputField: React.FC<IProps> = function ({
@@ -18,14 +19,22 @@ export const InputField: React.FC<IProps> = function ({
   errorMessage,
   onInputChange,
   textareaMode = false,
+  initialValue = "",
 }) {
   return (
     <div className={`${label && "my-4"}`}>
       {label && <Label secondary={secondaryLabel}>{label}</Label>}
       {!textareaMode && (
-        <Input type={inputType} onChange={onInputChange} error={showError} />
+        <Input
+          type={inputType}
+          onChange={onInputChange}
+          error={showError}
+          initialValue={initialValue}
+        />
       )}
-      {textareaMode && <Textarea onChange={onInputChange} />}
+      {textareaMode && (
+        <Textarea onChange={onInputChange} initialValue={initialValue} />
+      )}
       {showError && <Error>{errorMessage}</Error>}
     </div>
   );
@@ -41,7 +50,10 @@ const Label = function (props: any) {
 };
 
 const Input = function (props: any) {
-  const onChangeHandler = (event: any) => {
+  const [value, setValue] = useState(props.initialValue);
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
     props.onChange(event.currentTarget.value);
   };
 
@@ -54,13 +66,17 @@ const Input = function (props: any) {
       } font-body`}
       spellCheck="false"
       type={props.type}
+      value={value}
       onChange={onChangeHandler}
     />
   );
 };
 
 const Textarea = function (props: any) {
-  const onChangeHandler = (event: any) => {
+  const [value, setValue] = useState(props.initialValue);
+
+  const onChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.currentTarget.value);
     props.onChange(event.currentTarget.value);
   };
 
@@ -68,6 +84,7 @@ const Textarea = function (props: any) {
     <textarea
       className="h-24 min-w-full p-2 text-sm duration-200 bg-gray-100 border-gray-400 rounded outline-none appearance-none focus:shadow-outline focus:bg-white font-body focus:outline-none"
       onChange={onChangeHandler}
+      value={value}
     ></textarea>
   );
 };
