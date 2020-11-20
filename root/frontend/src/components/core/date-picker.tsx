320abc614aae2716118b8735f5dea8709a982548
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-20 10:30:44 +0530

    CHORE: Add initialValue prop to DatePicker
    This `initialValue` prop is used to initialize the internal value state.
    
    initialValue props is of type number. It actually datetime in utc milliseconds.
    
    moment.js is used to parse this number to readable date in string type.

--- root/frontend/src/components/core/date-picker.tsx
+++ root/frontend/src/components/core/date-picker.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 
 interface IProps extends React.HTMLAttributes<HTMLInputElement> {
   onSelectDate: (value: string) => void;
+  initialValue?: number;
 }
 
 export const DatePicker: React.FC<IProps> = function ({
   onSelectDate = (value: string) => {},
+  initialValue = moment.now(),
 }) {
+  const [value, setValue] = useState(
+    moment.utc(initialValue).format("YYYY-MM-DD")
+  );
+
   const handleStartingDate = (event: React.FormEvent<HTMLInputElement>) => {
     onSelectDate(event.currentTarget.value);
+    setValue(event.currentTarget.value);
   };
 
   return (
     <input
       type="date"
+      value={value}
       onChange={handleStartingDate}
       className="p-2 text-sm font-medium border rounded shadow-sm cursor-pointer font-body focus:outline-none"
     />
