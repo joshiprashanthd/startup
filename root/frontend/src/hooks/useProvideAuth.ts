1d56d233237208e72af344a499ae98a379ee6514
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-20 15:24:02 +0530

    CHORE: Add ME query logic to useProvideAuth

--- root/frontend/src/hooks/useProvideAuth.ts
+++ root/frontend/src/hooks/useProvideAuth.ts
@@ -1,13 +1,27 @@
-import { useMutation } from "@apollo/client";
-import { useCallback, useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import { useCallback, useEffect, useState } from "react";
 
 //local
 import { IAuthInfo } from "../contexts/auth-context";
 import { SIGN_OUT } from "../graphql/user/mutation";
+import { ME } from "../graphql/user/query";
 
 export const useProvideAuth = function () {
   const [user, setUser] = useState<IAuthInfo | null>(null);
+  const { data, loading } = useQuery(ME);
   const [mutate] = useMutation(SIGN_OUT);
+
+  useEffect(() => {
+    if (data) {
+      const user: IAuthInfo = {
+        id: data.me.id,
+        email: data.me.accountInfo.email,
+        handler: data.me.accountInfo.handler,
+        name: data.me.accountInfo.name,
+      };
+      signIn(user);
+    }
+  }, [data]);
 
   const signIn = (user: IAuthInfo, callback?: () => void): void => {
     setUser(user);
@@ -33,5 +47,6 @@ export const useProvideAuth = function () {
     signIn,
     signOut,
     signUp: null,
+    loading,
   };
 };
