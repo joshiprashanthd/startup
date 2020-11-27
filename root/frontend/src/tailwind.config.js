977f4eeedd43dde21618a3ca0b4d42372a958b87
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-27 13:33:22 +0530

    CHORE: Purge all tailwind base and utilities classes

--- root/frontend/src/tailwind.config.js
+++ root/frontend/src/tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ["./**/*.html", "./**/*.tsx", "./**/*.ts"],
+  },
   target: "relaxed",
   prefix: "",
   important: false,
