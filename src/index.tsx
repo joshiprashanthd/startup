d55f40d4b558c29223b89fa7f82b686efdfece34
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-11-27 21:44:04 +0530

    chore: set client uri accoring to node environment

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/styles.css";
 
+const clientUri =
+  process.env.NODE_ENV === "production"
+    ? "https://collabs1.herokuapp.com/graphql"
+    : "http://localhost:4000/graphql";
+
 const client = new ApolloClient({
-  uri: "https://collabs1.herokuapp.com/graphql",
+  uri: clientUri,
   cache: new InMemoryCache(),
   credentials: "include",
 });
